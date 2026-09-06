47157e8e892d7a8664036ee2189ae114b2ddd5df
AMANDEV TUDU <133139493+amandv123@users.noreply.github.com>
2026-09-06 19:48:51 +0530

    Remove contact info strip from page

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import Reviews from './components/Reviews'
 import Owner from './components/Owner'
 import Location from './components/Location'
 import Contact from './components/Contact'
-import ContactInfoStrip from './components/ContactInfoStrip'
 import Footer from './components/Footer'
 import MobileActionBar from './components/MobileActionBar'
 
@@ -26,7 +25,6 @@ export default function App() {
         <Owner />
         <Location />
         <Contact />
-        <ContactInfoStrip />
       </main>
       <Footer />
       <MobileActionBar />
